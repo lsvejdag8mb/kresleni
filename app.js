2526cf9a1a795ac208cb6bd58e5fca853bacb1dd
Libor Svejda <libor.svejda@gmail.com>
2020-06-09 14:16:56 +0200

    více hráčů online

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const uniqid = require("uniqid");
 
 const PORT = 8080;
+
+let hraci = new Array();
 
 function main(req,res) {
     if (req.url == "/") {
         res.writeHead(200, {"Content-type":"text/html"});
         res.end(fs.readFileSync("index.html"));
+    } else if (req.url.startsWith("/novyhrac")) {
+        let q = url.parse(req.url, true); //zpracuje parametry z url
+        let obj = {};
+        obj.uid = uniqid(); //do prohlizece se vrati identifikace hrace
+        res.writeHead(200, {"Content-type":"application/json"});
+        res.end(JSON.stringify(obj));
+        let hrac = {};
+        hrac.uid = obj.uid;
+        hrac.x = 100;
+        hrac.y = 100;
+        hrac.r = 10;
+        console.log(q.query);
+        hrac.jmeno = q.query.j; //podle parametru j
+        hrac.barva = "#" + q.query.b; //podle parametru b s pridanou mrizi
+        hraci.push(hrac);
     } else {
         res.writeHead(404);
         res.end();
@@ -25,19 +44,37 @@ const wss = new WebSocket.Server({ server: srv });
 //prijem zprav od klientu
 wss.on('connection', ws => {
     ws.on('message', message => { //prijem zprav
-        console.log(`Přijatá zpráva: ${message}`);
+        //console.log(`Přijatá zpráva: ${message}`);
+        let posunuti = JSON.parse(message);
+        for (let hrac of hraci) {
+            if (posunuti.uid == hrac.uid) { //vyhleda prislusneho hrace
+                if (posunuti.left) {
+                    hrac.x = hrac.x - 1;
+                }
+                if (posunuti.right) {
+                    hrac.x = hrac.x + 1;
+                }
+                if (posunuti.up) {
+                    hrac.y = hrac.y - 1;
+                }
+                if (posunuti.down) {
+                    hrac.y = hrac.y + 1;
+                }
+                break;
+            }
+        }
+
     });
 });
 
 //rozeslani zprav klientum
-let counter = 0;
 function broadcast() {
-    counter++;
+    let json = JSON.stringify(hraci);
     //odeslani zpravy vsem pripojenym klientum
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-            client.send("Webserver spuštěn " + counter + "s.");
+            client.send(json);
         }
     });
 }
-setInterval(broadcast, 1000);
+setInterval(broadcast, 10);
